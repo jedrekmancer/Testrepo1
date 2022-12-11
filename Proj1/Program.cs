ee7af63b85e00dcf7f35dd4d204b73d2ca1bb301
jedrekmancer <andrzej.kaganek@gmail.com>
2022-12-11 03:23:05 +0100

    2 proj 1st comm

---
+++ Proj1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proj1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Console.Write("Podaj silnię: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(a);
+            
+            
+            Console.Write(RecurSilnia(a));
+
+            Console.ReadLine();
+        }
+
+        public static int RecurSilnia(int b)
+        {
+            int silnia;
+            if (b == 0 || b == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                silnia = b * (b = b - 1);
+                Console.WriteLine(b);
+                Console.WriteLine(silnia);
+                return RecurSilnia(b);
+            
+            }
+            
+        }
+
+    }
+    
+   
+
+}
---
+++ ProjTest1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjTest1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b = 1;
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("b " + b);
+                a = b * (b =  b + 1); 
+                
+                Console.WriteLine("a " + a);
+                
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
